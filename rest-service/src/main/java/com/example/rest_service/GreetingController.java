6a556de1d3d49df65e4b625f2efa23bf62595d6a
juancanon1725 <98672541+juancanon1725@users.noreply.github.com>
2024-06-27 12:35:03 -0500

    Add files via upload

---
+++ rest-service/src/main/java/com/example/rest_service/GreetingController.java
@@ -0,0 +1,37 @@
+package com.example.rest_service;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    @RequestMapping(
+            value = "/hellopost",
+            method = RequestMethod.GET,
+            produces = "application/json"
+    )
+
+            public String status() {
+        return "{\"status\":\"Greetings from Spring Boot. "
+                + java.time.LocalDate.now() + ", "
+                + java.time.LocalTime.now()
+                + ". " + "The server is Runnig!\"}";
+    }
+
+    public String hellopost(@RequestParam(value = "name", defaultValue = "World") String name){
+        return String.format("Hello %s!", name);
+    }
+}
